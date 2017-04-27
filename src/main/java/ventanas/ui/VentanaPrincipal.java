b24ce3be84d47fde3a66a6a08a817572adcc9140
Maxi <maxi.fernandez.95@hotmail.com>
2017-04-27 13:03:32 -0300

    ventana principal a terminar

---
+++ src/main/java/ventanas/ui/VentanaPrincipal.java
@@ -0,0 +1,50 @@
+package ventanas.ui;
+
+import org.uqbar.arena.layout.HorizontalLayout;
+import org.uqbar.arena.layout.VerticalLayout;
+import org.uqbar.arena.widgets.Label;
+import org.uqbar.arena.widgets.Panel;
+import org.uqbar.arena.windows.SimpleWindow;
+import org.uqbar.arena.windows.WindowOwner;
+
+import ventanas.vm.VentanaPrincipalViewModel;
+
+
+public class VentanaPrincipal extends SimpleWindow<VentanaPrincipalViewModel> {
+
+	private static final long serialVersionUID = 1L;
+	
+	private WindowOwner parent;
+	private static VentanaPrincipalViewModel ventanaVM = new VentanaPrincipalViewModel();
+	
+	public VentanaPrincipal(WindowOwner parent) {
+		super(parent, ventanaVM);
+		this.parent = parent;
+		
+	}
+
+	@Override
+	protected void addActions(Panel actionsPanel) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void createFormPanel(Panel mainPanel) {
+		
+		this.setTitle("Ventana Principal");
+		mainPanel.setLayout(new HorizontalLayout());
+		Panel izqPanel = new Panel(mainPanel);
+		Panel derPanel = new Panel(mainPanel);
+		izqPanel.setLayout(new VerticalLayout());
+		derPanel.setLayout(new VerticalLayout());
+		new Label(izqPanel).setText("Empresas").setWidth(150);
+		new Label(derPanel).setText("Cuentas disponibles");
+		new Label(derPanel).setText("Resultado de aplicar indicadores en un periodo");
+		new Label(derPanel).setText("Graficar de periodo de la empresa seleccionada");
+	}
+	
+	public VentanaPrincipalViewModel getDatosVM() {
+		return ventanaVM;
+	}
+}
---
+++ src/main/java/ventanas/vm/VentanaPrincipalViewModel.java
@@ -0,0 +1,8 @@
+package ventanas.vm;
+
+import org.uqbar.commons.utils.Observable;
+
+@Observable
+public class VentanaPrincipalViewModel {
+
+}
