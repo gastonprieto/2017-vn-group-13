109a19ea90854432da866307a3391b07e077968b
BiancoN <bianco.nicolasm@gmail.com>
2017-05-10 21:37:34 -0300

    Arreglo un error al visualizar cuentas

--- src/main/java/model/Cuenta.java
+++ src/main/java/model/Cuenta.java
@@ -1,5 +1,8 @@
 package model;
 
+import org.uqbar.commons.utils.Observable;
+
+@Observable
 public class Cuenta {
 	private String name;
 	private Double value;
--- src/main/java/window/VerCuentasView.java
+++ src/main/java/window/VerCuentasView.java
@@ -2,6 +2,8 @@ package window;
 
 import org.uqbar.arena.bindings.PropertyAdapter;
 import org.uqbar.arena.layout.HorizontalLayout;
+import org.uqbar.arena.layout.VerticalLayout;
+import org.uqbar.arena.widgets.Label;
 import org.uqbar.arena.widgets.Panel;
 import org.uqbar.arena.widgets.Selector;
 import org.uqbar.arena.widgets.tables.Column;
@@ -31,7 +33,11 @@ public class VerCuentasView extends SimpleWindow<VerCuentasViewModel> {
 		this.setTitle("Ver Cuentas");
 		mainPanel.setLayout(new HorizontalLayout());
 		
-		Selector<Empresa> selector = new Selector<Empresa>(mainPanel);
+		Panel verticalPanel = new Panel(mainPanel).setLayout(new VerticalLayout());
+		
+		new Label(verticalPanel).setText("Seleccione una empresa: ");
+		
+		Selector<Empresa> selector = new Selector<Empresa>(verticalPanel);
 		selector.allowNull(false);
 		selector.bindValueToProperty("empresaSeleccionada");
 		selector.bindItemsToProperty("empresas").setAdapter(new PropertyAdapter(Empresa.class, "name"));
@@ -40,8 +46,9 @@ public class VerCuentasView extends SimpleWindow<VerCuentasViewModel> {
 		table.bindItemsToProperty("empresaSeleccionada.cuentas");
 
 		new Column<Cuenta>(table).setTitle("Nombre").setFixedSize(150).bindContentsToProperty("name");
-		new Column<Cuenta>(table).setTitle("Valor").setFixedSize(150).bindContentsToProperty("value");
-		new Column<Cuenta>(table).setTitle("Año").setFixedSize(150).bindContentsToProperty("periodo.year");
+		new Column<Cuenta>(table).setTitle("Valor").setFixedSize(75).bindContentsToProperty("value");
+		new Column<Cuenta>(table).setTitle("Año").setFixedSize(75).bindContentsToProperty("periodo.year");
+		new Column<Cuenta>(table).setTitle("Semestre").setFixedSize(75).bindContentsToProperty("periodo.semester");
 	}
 }
 
