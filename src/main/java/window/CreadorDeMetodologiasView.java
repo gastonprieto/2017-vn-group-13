a619cf4e24183c229178720c8194ec5537797994
Maxxiii95 <maxi.fernandez.95@hotmail.com>
2017-07-07 17:46:10 -0300

    pantalla metodologias

--- src/main/java/window/CreadorDeMetodologiasView.java
+++ src/main/java/window/CreadorDeMetodologiasView.java
@@ -1,5 +1,7 @@
 package window;
 
+import java.util.Collection;
+
 import org.uqbar.arena.bindings.PropertyAdapter;
 import org.uqbar.arena.layout.HorizontalLayout;
 import org.uqbar.arena.layout.VerticalLayout;
@@ -7,14 +9,16 @@ import org.uqbar.arena.widgets.Button;
 import org.uqbar.arena.widgets.Label;
 import org.uqbar.arena.widgets.Panel;
 import org.uqbar.arena.widgets.Selector;
+import org.uqbar.arena.widgets.Spinner;
 import org.uqbar.arena.widgets.TextBox;
 import org.uqbar.arena.windows.SimpleWindow;
 import org.uqbar.arena.windows.WindowOwner;
 
+import model.Condicion;
 import model.Indicador;
-import viewmodel.CreadorDeMetodologiasYCondicionesViewModel;
+import viewmodel.CreadorDeMetodologiasViewModel;
 
-public class CreadorDeMetodologiasYCondicionesView extends SimpleWindow<CreadorDeMetodologiasYCondicionesViewModel> {
+public class CreadorDeMetodologiasView extends SimpleWindow<CreadorDeMetodologiasViewModel> {
 	
 	private static final long serialVersionUID = 1L;
 	
@@ -25,51 +29,66 @@ public class CreadorDeMetodologiasYCondicionesView extends SimpleWindow<CreadorD
 	private Button cancelar;
 	
 	Selector selectorTipos;
+	Selector<String> selectorModo;
+	Collection<Condicion> condiciones;
 	
 	WindowOwner parent;
 	
-	private static CreadorDeMetodologiasYCondicionesViewModel creadorVM = new CreadorDeMetodologiasYCondicionesViewModel();	
+	private static CreadorDeMetodologiasViewModel creadorVM = new CreadorDeMetodologiasViewModel();	
 	
-	public CreadorDeMetodologiasYCondicionesView(WindowOwner parent) {
+	public CreadorDeMetodologiasView(WindowOwner parent) {
 		super(parent, creadorVM);
 		this.parent = parent;
 	}
 
 	@Override
 	protected void createFormPanel(Panel mainPanel) {
-		this.setTitle("Creador de indicadores");
+		this.setTitle("Creador de Metodologia");
 		
 		mainPanel.setLayout(new HorizontalLayout());
-		Panel panel = new Panel(mainPanel).setLayout(new VerticalLayout());
+		Panel panel1 = new Panel(mainPanel).setLayout(new VerticalLayout());
 		Panel panel2 = new Panel(mainPanel).setLayout(new VerticalLayout());
 		Panel panel3 = new Panel(mainPanel).setLayout(new VerticalLayout());
 		Panel panel4 = new Panel(mainPanel).setLayout(new VerticalLayout());
+		Panel panel5 = new Panel(mainPanel).setLayout(new VerticalLayout());
 		
-		new Label(panel).setText("Seleccione el tipo: ");
-		selectorTipos = new Selector(panel);
+		new Label(panel1).setText("Seleccione el tipo: ");
+		Selector selectorTipos = new Selector(panel1);
 		selectorTipos.allowNull(false);
 		selectorTipos.bindItemsToProperty("tipos");
 		selectorTipos.bindValueToProperty("tipoCondicionSeleccionado");		
-			
-		new Label(panel2).setText("Seleccione el indicador: ");
-		Selector selectorIndicadores = new Selector(panel2);
+		
+		new Label(panel2).setText("Seleccione el Modo: ");
+		Selector<String> selectorModo = new Selector<String>(panel2);
+		selectorModo.allowNull(false);
+		selectorModo.bindItemsToProperty("modo");
+		selectorModo.bindValueToProperty("modoCondicionSeleccionado");
+		
+
+		
+		new Label(panel3).setText("Seleccione el indicador: ");
+		Selector<Indicador> selectorIndicadores = new Selector<Indicador>(panel3);
 		selectorIndicadores.allowNull(false);
 		selectorIndicadores.bindValueToProperty("indicadorSeleccionado");
 		selectorIndicadores.bindItemsToProperty("indicadores").setAdapter(new PropertyAdapter(Indicador.class, "nombre"));
 		
-		new Label(panel3).setText("Seleccione periodo: ").setWidth(150);
-		new TextBox(panel3);		
+		new Label(panel4).setText("Seleccione periodo: ").setWidth(150);
 		
-		cancelar = new Button(panel).setCaption("Cancelar");
-		siguiente = new Button(panel2).setCaption("Siguiente");
-		finalizar = new Button(panel3).setCaption("Finalizar");
-		volverAlMenu = new Button(panel4).setCaption("Volver al menu");
+		Spinner periodo = new Spinner(panel4); 
+		periodo.bindValueToProperty("periodoSeleccionado");
+		
+		cancelar = new Button(panel1).setCaption("Cancelar");
+		siguiente = new Button(panel3).setCaption("Siguiente");
+		finalizar = new Button(panel4).setCaption("Finalizar");
+		volverAlMenu = new Button(panel5).setCaption("Volver al menu");
 	}
 	
 	@Override
 	protected void addActions(Panel actionsPanel) {
 		volverAlMenu.onClick(this::volverAlMenu);
+		siguiente.onClick(this::agregarCondicion);
 		selectorTipos.onSelection(this::seleccionarTipo);
+		selectorModo.onSelection(this::seleccionarModo);
 	}
 	
 	public void volverAlMenu() {
@@ -82,9 +101,15 @@ public class CreadorDeMetodologiasYCondicionesView extends SimpleWindow<CreadorD
 		System.out.println(creadorVM.getTipoCondicionSeleccionado());
 	}
 	
-	public CreadorDeMetodologiasYCondicionesViewModel getCreadorVM() {
+	public void seleccionarModo() {
+		System.out.println(creadorVM.getModoCondicionSeleccionado());
+	}
+	public CreadorDeMetodologiasViewModel getCreadorVM() {
 		return creadorVM;
 	}
 	
+	public void agregarCondicion(){
+		
+	}
 
 }
