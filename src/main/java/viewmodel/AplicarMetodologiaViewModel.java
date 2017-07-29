563e12743d9e1dac04e4308c3edf02580a65363b
evguenitessov <tessov.evgueni@hotmail.com>
2017-07-29 20:13:13 -0300

    - Se agrega metodologia para aplicar.

--- src/main/java/viewmodel/AplicarMetodologiaViewModel.java
+++ src/main/java/viewmodel/AplicarMetodologiaViewModel.java
@@ -1,9 +1,15 @@
 package viewmodel;
 
+import model.Condicion;
+import model.CondicionMayor;
 import model.Empresa;
+import model.Indicador;
 import model.Metodologia;
 import model.RepositorioDeEmpresas;
 import model.RepositorioDeMetodologias;
+import model.Sumatoria;
+import utils.InterpretadorDeIndicadores;
+
 import org.uqbar.commons.utils.Observable;
 
 import java.text.CollationElementIterator;
@@ -17,6 +23,18 @@ import java.util.concurrent.ArrayBlockingQueue;
 
 @Observable
 public class AplicarMetodologiaViewModel {
+	
+	public AplicarMetodologiaViewModel() {
+		InterpretadorDeIndicadores interprete = new InterpretadorDeIndicadores();		
+		Indicador indicadorMayor = interprete.interpretar("ROE","2+Cuenta 1");
+		
+		Condicion condicion1 = new CondicionMayor("CondicionMayor1", 20, 10, new Sumatoria(indicadorMayor));
+		Collection<Condicion> condiciones = new ArrayList();
+		condiciones.add(condicion1);
+		
+		Metodologia metodologia = new Metodologia("WB", condiciones);
+		RepositorioDeMetodologias.getInstance().registrarMetodologia(metodologia);
+	}
 
     private Metodologia metodologiaSeleccionada;
     private Collection<Empresa> resultadoEmpresasEvaluadas = new ArrayList<>();
