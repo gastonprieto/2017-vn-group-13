f280f37497c8023a937f76d24e2337805922957a
evguenitessov <tessov.evgueni@hotmail.com>
2017-07-29 17:15:34 -0300

    - Se agrega metodos de creacion de la fabrica de Condicion.

--- src/main/java/model/Fabrica.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-/**
- * Created by rapap on 29/07/2017.
- */
-public interface Fabrica {
-
-
-}
--- src/main/java/model/FabricaCondicion.java
+++ src/main/java/model/FabricaCondicion.java
@@ -3,5 +3,14 @@ package model;
 /**
  * Created by rapap on 29/07/2017.
  */
-public abstract class FabricaCondicion implements Fabrica{
+public abstract class FabricaCondicion{
+	
+	public Condicion ObtenerCondicion()
+    {
+        Condicion condicion = CrearCondicion();
+        
+        return condicion;
+    }
+	
+	protected abstract Condicion CrearCondicion();
 }
