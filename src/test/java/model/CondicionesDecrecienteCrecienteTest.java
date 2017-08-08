650a8227f72897d308fcda8ea68c0e35703d42ef
rapaportmatias <rapaport.matias@gmail.com>
2017-08-08 00:32:05 -0200

    el test tira error AssertionError

--- src/test/java/model/CondicionesDecrecienteCrecienteTest.java
+++ src/test/java/model/CondicionesDecrecienteCrecienteTest.java
@@ -17,103 +17,40 @@ import java.util.stream.Stream;
 public class CondicionesDecrecienteCrecienteTest {
     ImportadorDeDatos importador;
 
-    Empresa retornoEsperadoEmpresa;
-
     InterpretadorDeIndicadores interprete ;
 
     FabricaCondicion fabrica;
-    private Periodo periodoUnico;
-    private Collection<Periodo> periodos;
 
-    private Cuenta cuentaMayor;
-    private Cuenta cuentaMenor;
-
-    private ArrayList<Cuenta> listCuentasMayor;
-    private ArrayList<Cuenta> listCuentasMenor;
-
-    private Empresa empresaMayor;
-    private Empresa empresaMenor;
-
-    private ArrayList<Empresa> listaEmpresas;
-    private ArrayList<Empresa> listaEmpresasMeyor;
-    private ArrayList<Empresa> listaEmpresasMenor;
 
     Stream<Empresa> streamEmpresas;
-    Stream<Empresa> streamEmpresasMayor;
+    Stream<Empresa> streamEmpresasResultadoCreciente;
     Stream<Empresa> streamEmpresasMenor;
 
-    Indicador indicadorMayor;
-    Indicador indicadorMenor;
+    Indicador indicadorCreciente;
+    Indicador indicadorDecreciente;
 
-    private Condicion condicionMayor;
-    private Condicion condicionMenor;
+    private Condicion condicionCreciente;
+    private Condicion condicionDecreciente;
 
     @Before
     public void setUp() {
 
-
-        //fabrica = new FabricaCondicionesDePrioridad("", , 10, "");
-
-
-	    //fabrica = new FabricaCondicionTaxativas(nombreCondicion, tipoIndicadorSeleccionada, indicadorSeleccionado, 10, tipoCondicionSeleccionada, 10.1);
-
-
-        /*
+        //Lleno el repositrio de Empresas
         importador = new ImportadorDeDatos();
         importador.importarRepositorioDeEmpresas(System.getProperty("user.dir") + "/src/test/assets/Cuentas.txt");
 
-        periodoUnico = new Periodo();
-        periodoUnico.setSemester(1);
-        periodoUnico.setYear(2016);
-
-        cuentaMayor = new Cuenta();
-        cuentaMayor.setName("CuentaMayor");
-        cuentaMayor.setValue(20.1);
-        cuentaMayor.setPeriodo(periodoUnico);
-
-        cuentaMenor = new Cuenta();
-        cuentaMenor.setName("CuentaMenor");
-        cuentaMenor.setValue(1.1);
-        cuentaMenor.setPeriodo(periodoUnico);
-
-        listCuentasMayor = new ArrayList<Cuenta>();
-        listCuentasMayor.add(cuentaMayor);
-
-        listCuentasMenor = new ArrayList<Cuenta>();
-        listCuentasMenor.add(cuentaMenor);
-
-        empresaMayor = new Empresa();
-        empresaMayor.setName("EmpresaMayor");
-        empresaMayor.setCuentas(listCuentasMayor);
-
-        empresaMenor = new Empresa();
-        empresaMenor.setName("EmpresaMenor");
-        empresaMenor.setCuentas(listCuentasMenor);
-
-        listaEmpresas = new ArrayList<Empresa>();
-        listaEmpresas.add(empresaMayor);
-        listaEmpresas.add(empresaMenor);
-
-        listaEmpresasMeyor = new ArrayList<Empresa>();
-        listaEmpresasMeyor.add(empresaMayor);
-
-        listaEmpresasMenor = new ArrayList<Empresa>();
-        listaEmpresasMenor.add(empresaMenor);
-
-        periodos = new ArrayList<Periodo>();
-        periodos.add(periodoUnico);
-
-        streamEmpresas = listaEmpresas.stream();
-        streamEmpresasMayor = listaEmpresasMenor.stream();
-        streamEmpresasMenor = listaEmpresasMenor.stream();
-
         interprete = new InterpretadorDeIndicadores();
 
-        indicadorMayor = interprete.interpretar("IndicadorMayor","CuentaMayor+1");
-        indicadorMenor = interprete.interpretar("indicadorMenor", "CuentaMenor+1");
+        indicadorCreciente = interprete.interpretar("Indicador1",    "Cuenta1-1");
+        indicadorDecreciente = interprete.interpretar("indicador2", "Cuenta1+1");
 
-        condicionMayor = new CondicionMayor("UnaCondicionMayor",8.8, indicadorMayor, periodos);
-        condicionMenor = new CondicionMenor("hla",8.8, indicadorMenor, periodos);*/
+        fabrica = new FabricaCondicionesDePrioridad("CondicionCreciente", indicadorCreciente  , 2, "Creciente");
+        condicionCreciente = fabrica.ObtenerCondicion();
+
+        fabrica = new FabricaCondicionesDePrioridad("CondicionCreciente", indicadorDecreciente , 1, "Decreciente");
+        condicionDecreciente = fabrica.ObtenerCondicion();
+
+        streamEmpresasResultadoCreciente = RepositorioDeEmpresas.getInstance().getEmpresas().stream();
 
     }
 
@@ -123,10 +60,23 @@ public class CondicionesDecrecienteCrecienteTest {
     }
 
     @Test
-    public void AplicarMayorTest()  throws ParserException {
-        Stream<Empresa> Resultado =  condicionMayor.aplicar(streamEmpresas);
-        Assert.assertEquals(Resultado.collect(Collectors.toList()), streamEmpresasMayor.collect(Collectors.toList()));
+    public void AplicarCrecienteTest()  throws ParserException {
+        Stream<Empresa> Resultado =  condicionCreciente.aplicar(RepositorioDeEmpresas.getInstance().getEmpresas().stream());
+        Assert.assertTrue(Resultado.equals(streamEmpresasResultadoCreciente));
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
