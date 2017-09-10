a8db81870b35efbcd2423dbed867608010c8fd86
Nicolas Bianco <bianco.nicolasm@gmail.com>
2017-09-10 16:33:50 -0300

    CondicionPrioritaria ordena una lista de empresas, acepta condicion de desempate y requiere forma de aplicacion

--- src/main/java/model/Condicion/Prioridad/CondicionPrioritaria.java
+++ src/main/java/model/Condicion/Prioridad/CondicionPrioritaria.java
@@ -1,42 +1,59 @@
 package model.Condicion.Prioridad;
 
-
 import model.Empresa;
 import model.Indicador;
 import model.Periodo;
+import model.formasDeAplicacion.FormaAplicacion;
+
 import org.apache.commons.lang.StringUtils;
 
 import java.util.Collection;
-
-/**
- * Created by rapap on 27/07/2017.
- */
-
+import java.util.List;
+import java.util.stream.Collectors;
 
 public abstract class CondicionPrioritaria  {
-    protected String name;
-    protected Indicador indicador;
-    protected int cantidadDePeriodos;
-
-
-    public abstract int comparar(Empresa empresa1, Empresa empresa2, Collection<Periodo> periodos);
-
-    public String getClassClean(){
-        String[] myClass = StringUtils.splitByWholeSeparator( this.getClass().toString(), ".");
-        return myClass[myClass.length-1];
-    }
-    public String getName() {
-        return name;
-    }
-
-
-    public Indicador getIndicador() {
-        return indicador;
-    }
-
-
-    public int getCantidadDePeriodos() {
-        return cantidadDePeriodos;
-    }
+	
+	private Indicador indicador;
+	private CondicionPrioritaria condicionDesempate;
+	private FormaAplicacion formaAplicacion;
+	
+	public List<Empresa> ordenar(List<Empresa> empresas) {
+		return empresas.stream().sorted((empresa1, empresa2) -> this.realizarComparacion(empresa1, empresa2)).collect(Collectors.toList());
+	}
+	
+	public int realizarComparacion(Empresa empresa1, Empresa empresa2) {
+		int resultado = this.formaAplicacion.aplicar(this, empresa1, empresa2);
+		if(resultado == 0 && condicionDesempate != null) {
+			return condicionDesempate.realizarComparacion(empresa1, empresa2);
+		}
+		return resultado;
+	}
+	
+	public abstract int comparar(Empresa empresa1, Empresa empresa2);
+	
+//    protected String name;
+//    protected Indicador indicador;
+//    protected int cantidadDePeriodos;
+//
+//
+//    public abstract int comparar(Empresa empresa1, Empresa empresa2, Collection<Periodo> periodos);
+//
+//    public String getClassClean(){
+//        String[] myClass = StringUtils.splitByWholeSeparator( this.getClass().toString(), ".");
+//        return myClass[myClass.length-1];
+//    }
+//    public String getName() {
+//        return name;
+//    }
+//
+//
+//    public Indicador getIndicador() {
+//        return indicador;
+//    }
+//
+//
+//    public int getCantidadDePeriodos() {
+//        return cantidadDePeriodos;
+//    }
 
 }
