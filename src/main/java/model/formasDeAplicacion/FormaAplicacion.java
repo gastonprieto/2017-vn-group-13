a156e1216e8322fe6f0bca2de325b98613cdb86c
Nicolas Bianco <bianco.nicolasm@gmail.com>
2017-09-10 17:40:58 -0300

    Agrego interfaz FormaAplicacion

---
+++ src/main/java/model/formasDeAplicacion/FormaAplicacion.java
@@ -0,0 +1,9 @@
+package model.formasDeAplicacion;
+
+import model.Empresa;
+import model.Condicion.Prioridad.CondicionPrioritaria;
+
+public interface FormaAplicacion {
+	
+	int aplicar(CondicionPrioritaria condicionPrioritaria, Empresa empresa1, Empresa empresa2);
+}
