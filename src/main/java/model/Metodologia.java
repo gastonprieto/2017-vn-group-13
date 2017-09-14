7f97faffebb039812b15e005a12456a19dc62600
BiancoN <bianco.nicolasm@gmail.com>
2017-09-13 23:49:09 -0300

    Creo pantallas de creacion de metodologia y condicion

--- src/main/java/model/Metodologia.java
+++ src/main/java/model/Metodologia.java
@@ -8,7 +8,6 @@ import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
